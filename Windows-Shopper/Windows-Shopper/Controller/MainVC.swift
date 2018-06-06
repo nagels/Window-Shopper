cb9ad473d368b8907883994fdf37d9aa34da4bb1
Frederik Nagels <nagels97@live.dk>
2018-06-07 00:13:16 +0200

    Lesson02

---
+++ Windows-Shopper/Windows-Shopper/Controller/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Windows-Shopper
+//
+//  Created by Full On Pimp on 06/06/2018.
+//  Copyright © 2018 Nagels. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9717252267, green: 0.5619667579, blue: 0.008637083771, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    
+    @objc func calculate() {
+        
+    }
+
+}
+
