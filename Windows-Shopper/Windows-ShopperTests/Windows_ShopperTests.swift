99720fb72acaf04b33fd7a2d983dd2ae6353ab57
Frederik Nagels <nagels97@live.dk>
2018-06-06 21:29:14 +0200

    first commit

---
+++ Windows-Shopper/Windows-ShopperTests/Windows_ShopperTests.swift
@@ -0,0 +1,36 @@
+//
+//  Windows_ShopperTests.swift
+//  Windows-ShopperTests
+//
+//  Created by Full On Pimp on 06/06/2018.
+//  Copyright © 2018 Nagels. All rights reserved.
+//
+
+import XCTest
+@testable import Windows_Shopper
+
+class Windows_ShopperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
