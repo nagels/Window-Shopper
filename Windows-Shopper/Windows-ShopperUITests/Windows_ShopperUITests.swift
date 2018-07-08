fc9cb82f89348b851cf02d40556eb96ebe25fd6a
Frederik Nagels <nagels97@live.dk>
2018-07-08 23:56:55 +0200

    DrawRect

--- Windows-Shopper/Windows-ShopperUITests/Windows_ShopperUITests.swift
+++ Windows-Shopper/Windows-ShopperUITests/Windows_ShopperUITests.swift
@@ -32,5 +32,9 @@ class Windows_ShopperUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    func testGetHour(){
+    XCTAssert(Wage.getHours(forWage: 25, andPrice: 100) == 4)
+    XCTAssert(Wage.getHours(forWage: 15.50, andPrice: 250.53) == 17)
+    }
     
 }
